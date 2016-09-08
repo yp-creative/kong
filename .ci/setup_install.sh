86095ae933638c2ade17c15644cf4eb895e89f84
kensou97 <314830391@qq.com>
2016-09-08 17:55:29 +0800

    install script

--- .ci/setup_install.sh
+++ .ci/setup_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# 1.安装pcre openssl等
+# 安装pcre openssl等
 
 if ["" -eq `which yum`]; then
     # Debian 和 Ubuntu 用户
@@ -8,15 +8,15 @@ if ["" -eq `which yum`]; then
     apt-get install vim git unzip gcc perl libpcre3 libpcre3-dev openssl libssl-dev libreadline-gplv2-dev libncurses5-dev uuid-dev build-essential luajit
 else
     # Fedora 和 RedHat 用户
-    sudo yum install -y pcre openssl readline-devel pcre-devel openssl-devel gcc libuuid libuuid-devel mcrypt libmcrypt-devel
+    yum install -y pcre openssl readline-devel pcre-devel openssl-devel gcc libuuid libuuid-devel mcrypt libmcrypt-devel
 fi
 
-# 2.下载dyups
+# 下载dyups
 
 cd /opt
 git clone git://github.com/yzprofile/ngx_http_dyups_module.git
 
-# 4.下载并安装openresty
+# 下载并安装openresty
 # --prefix=/usr/local/openresty 程序会被安装到/usr/local/openresty目录
 
 wget http://openresty.org/download/ngx_openresty-1.9.15.1.tar.gz
@@ -52,7 +52,19 @@ make install
 
 cd /opt
 curl https://raw.githubusercontent.com/yp-creative/kong/develop/kong-0.8.3-0.rockspec > /usr/local/kong/plugins/kong-0.8.3-0.rockspec
-curl https://raw.githubusercontent.com/yp-creative/kong/develop/kong-custom-plugins-0.8.3-0.rockspec > /usr/local/kong/plugins/kong-custom-plugins-0.8.3-0.rockspec
 
 luarocks install ./kong-0.8.3-0.rockspec
-luarocks install ./kong-custom-plugins-0.8.3-0.rockspec
+
+#codec
+cd /opt
+git clone https://github.com/yp-creative/yop-nginx-codec.git
+cd yop-nginx-codec
+make
+sudo make install
+
+#mcrypt
+cd /opt
+git clone https://github.com/yp-creative/yop-nginx-mcrypt.git
+cd yop-nginx-mcrypt
+make
+sudo make install
